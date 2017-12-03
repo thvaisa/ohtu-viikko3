37476e7abca685e2bc4cd4dd14809036ae094cb1
timvaisa <thvaisa@gmail.com>
2017-12-03 20:30:53 +0200

    tehtava2

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -4,8 +4,8 @@ public class TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,68 +13,53 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
+    private String scoreToString(int score){
+        switch (score){
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "Deuce";
+        }        
+    }
+   
+    private String scoreToStringDraw(int score){
+        if(score<4 && score>=0){
+            return scoreToString(score)+"-All";
+        }
+        return scoreToString(score);
+    }
+   
+    private String whoIsWinning(int scoreDifference){
+        if (scoreDifference==1) return "Advantage player1";
+        else if (scoreDifference ==-1) return "Advantage player2";
+        else if (scoreDifference>=2) return "Win for player1";
+        return "Win for player2";
+    }
+    
+
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1==m_score2){
+            return scoreToStringDraw(m_score1);
+        }else if (m_score1>=4 || m_score2>=4){
+            return whoIsWinning(m_score1-m_score2);
+        }else{
+            score = scoreToString(m_score1);
+            score += "-"+scoreToString(m_score2);
+            return score;
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
